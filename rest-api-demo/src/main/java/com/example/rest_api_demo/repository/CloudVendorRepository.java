08cd96d7c74529120e14f0fb596fb0841a1cb859
PUNAMRAMPUKALE <p_pukale@u.pacific.edu>
2025-06-17 00:50:36 -0700

    ServiceLayer implementation done

--- rest-api-demo/src/main/java/com/example/rest_api_demo/repository/CloudVendorRepository.java
+++ rest-api-demo/src/main/java/com/example/rest_api_demo/repository/CloudVendorRepository.java
@@ -3,7 +3,7 @@ package com.example.rest_api_demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.rest_api_demo.model.CloudVendor;
-
+//This is the layer actully talking to database
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
 
     //<CloudVendor, String>
--- rest-api-demo/src/main/java/com/example/rest_api_demo/service/impl/CloudVendorServiceImpl.java
+++ rest-api-demo/src/main/java/com/example/rest_api_demo/service/impl/CloudVendorServiceImpl.java
@@ -1,5 +1,51 @@
 package com.example.rest_api_demo.service.impl;
 
-public class CloudVendorServiceImpl {
+import java.util.List;
+
+import com.example.rest_api_demo.model.CloudVendor;
+import com.example.rest_api_demo.repository.CloudVendorRepository;
+import com.example.rest_api_demo.service.CloudVendorService;
+
+public class CloudVendorServiceImpl implements CloudVendorService {
+
+//Initiated a cloudVendorRepository and created a constructor
+CloudVendorRepository cloudVendorRepository;
+
+CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository){
+    this.cloudVendorRepository=cloudVendorRepository;
+}
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+
+        //data of CloudVendor will get saved via Repository Layer into database
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        
+        cloudVendorRepository.save(cloudVendor);
+        return "Successfully updated";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        
+        cloudVendorRepository.deleteById(cloudVendorId); 
+        return "successfully deleted";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+       
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendor() {
+        return cloudVendorRepository.findAll();
+    }
 
 }
