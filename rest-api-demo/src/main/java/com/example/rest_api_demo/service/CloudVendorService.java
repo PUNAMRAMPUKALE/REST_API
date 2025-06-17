e6fc355d88c642b7f2ce2e6e448e720f6f343b51
PUNAMRAMPUKALE <p_pukale@u.pacific.edu>
2025-06-17 00:25:29 -0700

    Service/Business interface methods defined and Service class created for implementation

--- rest-api-demo/src/main/java/com/example/rest_api_demo/service/CloudVendorService.java
+++ rest-api-demo/src/main/java/com/example/rest_api_demo/service/CloudVendorService.java
@@ -1,5 +1,15 @@
 package com.example.rest_api_demo.service;
 
+import java.util.List;
+
+import com.example.rest_api_demo.model.CloudVendor;
+
 public interface CloudVendorService {
+public String createCloudVendor(CloudVendor cloudVendor);
+public String updateCloudVendor(CloudVendor cloudVendor);
+public String deleteCloudVendor(String cloudVendorId);
+public CloudVendor getCloudVendor(String cloudVendorId);
+public List<CloudVendor> getAllCloudVendor();
+
 
 }
---
+++ rest-api-demo/src/main/java/com/example/rest_api_demo/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.rest_api_demo.service.impl;
+
+public class CloudVendorServiceImpl {
+
+}
