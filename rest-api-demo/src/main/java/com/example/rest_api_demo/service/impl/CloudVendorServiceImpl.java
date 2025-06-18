14d22426b6778c2c831b6d7e44099b761e82d718
PUNAMRAMPUKALE <p_pukale@u.pacific.edu>
2025-06-18 03:35:04 -0700

    Formatting done

--- rest-api-demo/src/main/java/com/example/rest_api_demo/service/impl/CloudVendorServiceImpl.java
+++ rest-api-demo/src/main/java/com/example/rest_api_demo/service/impl/CloudVendorServiceImpl.java
@@ -8,22 +8,23 @@ import com.example.rest_api_demo.repository.CloudVendorRepository;
 import com.example.rest_api_demo.service.CloudVendorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 @Service
 public class CloudVendorServiceImpl implements CloudVendorService {
 
-//Initiated a cloudVendorRepository and created a constructor
+    // Initiated a cloudVendorRepository and created a constructor
 
-CloudVendorRepository cloudVendorRepository;
-private static final Logger logger = LoggerFactory.getLogger(CloudVendorServiceImpl.class);
+    CloudVendorRepository cloudVendorRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CloudVendorServiceImpl.class);
 
-CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository){
-    this.cloudVendorRepository=cloudVendorRepository;
-}
+    CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
 
     @Override
     public String createCloudVendor(CloudVendor cloudVendor) {
 
-        //data of CloudVendor will get saved via Repository Layer into database
+        // data of CloudVendor will get saved via Repository Layer into database
         try {
             cloudVendorRepository.save(cloudVendor);
             return "Success";
@@ -36,31 +37,31 @@ CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository){
     @Override
     public String updateCloudVendor(CloudVendor cloudVendor) {
         if (!cloudVendorRepository.existsById(cloudVendor.getVendorId()))
-        throw new CloudVendorNotFoundException("Cloud Vendor not found for update");
+            throw new CloudVendorNotFoundException("Cloud Vendor not found for update");
         cloudVendorRepository.save(cloudVendor);
         return "Successfully updated";
     }
 
     @Override
-    public String deleteCloudVendor(String cloudVendorId) {   
+    public String deleteCloudVendor(String cloudVendorId) {
         if (!cloudVendorRepository.existsById(cloudVendorId))
-        throw new CloudVendorNotFoundException("Cloud Vendor not found for deletion");
-        cloudVendorRepository.deleteById(cloudVendorId); 
+            throw new CloudVendorNotFoundException("Cloud Vendor not found for deletion");
+        cloudVendorRepository.deleteById(cloudVendorId);
         return "successfully deleted";
     }
 
     @Override
     public CloudVendor getCloudVendor(String cloudVendorId) {
-       if(cloudVendorRepository.findById(cloudVendorId).isEmpty())
-       throw new CloudVendorNotFoundException("Requested Cloud Vendor does not exist");
-       return cloudVendorRepository.findById(cloudVendorId).get();
+        if (cloudVendorRepository.findById(cloudVendorId).isEmpty())
+            throw new CloudVendorNotFoundException("Requested Cloud Vendor does not exist");
+        return cloudVendorRepository.findById(cloudVendorId).get();
     }
 
     @Override
     public List<CloudVendor> getAllCloudVendor() {
-       if(cloudVendorRepository.findAll().isEmpty())
-       throw new CloudVendorNotFoundException("No Cloud Vendor present");
-       return cloudVendorRepository.findAll();
+        if (cloudVendorRepository.findAll().isEmpty())
+            throw new CloudVendorNotFoundException("No Cloud Vendor present");
+        return cloudVendorRepository.findAll();
     }
 
 }
